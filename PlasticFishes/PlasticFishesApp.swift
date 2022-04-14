66b33d04856a1d98bd0f51d8f34cc32147d8c173
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-04-13 23:30:34 -0500

    Initial Commit

---
+++ PlasticFishes/PlasticFishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlasticFishesApp.swift
+//  PlasticFishes
+//
+//  Created by Luis Ezcurdia on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PlasticFishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
