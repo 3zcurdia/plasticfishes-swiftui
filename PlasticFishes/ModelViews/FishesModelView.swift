8d0c5a19895d8f4e71e416fdad02ae0216dc7397
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-04-14 12:47:45 -0500

    Remove prints on view model

--- PlasticFishes/ModelViews/FishesModelView.swift
+++ PlasticFishes/ModelViews/FishesModelView.swift
@@ -10,17 +10,12 @@ import Foundation
 class FishesViewModel: ObservableObject {
     @Published var fishes: [Fish] = []
 
-    init() {
-        print("Initialized FishesVM")
-    }
-
     @MainActor
     func fetch() async {
         let url = URL(string: "https://plasticfishes.herokuapp.com/api/fishes")!
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             self.fishes = try JSONDecoder().decode([Fish].self, from: data)
-            print(self.fishes)
         } catch {
             print("Oups an error occur!!")
         }
